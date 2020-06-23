a1254e3ff92cb86263372901defb33df75827e73
James Malone <jamesm28@outlook.com>
2020-06-22 18:49:06 -0700

    Adding featured description

--- app/controllers/serieseach.js
+++ app/controllers/serieseach.js
@@ -7,7 +7,7 @@ import {inject as service} from '@ember/service';
 import {slugify} from '@tryghost/string';
 import {task, timeout} from 'ember-concurrency';
 
-const SCRATCH_PROPS = ['name', 'slug', 'description', 'metaTitle', 'metaDescription'];
+const SCRATCH_PROPS = ['name', 'slug', 'description', 'metaTitle', 'metaDescription', 'featuredDescription'];
 
 export default Controller.extend({
     notifications: service(),
