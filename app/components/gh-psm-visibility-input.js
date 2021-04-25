bdcd35d599fe80374d5164af74262cd8b9312323
DESKTOP-HKTQQ3B\stvng <stvngrsh@gmail.com>
2021-04-25 16:00:31 -0400

    Add tiers to config

--- app/components/gh-psm-visibility-input.js
+++ app/components/gh-psm-visibility-input.js
@@ -1,32 +1,42 @@
-import Component from '@ember/component';
-import {computed} from '@ember/object';
-import {inject as service} from '@ember/service';
-
-const VISIBILITIES = [
-    {label: 'Public', name: 'public'},
-    {label: 'Platinum Penguinns', name: 'members'},
-    {label: 'Platinum Penguinns PLUS', name: 'paid'}
-];
+import Component from "@ember/component";
+import { computed } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
-
     settings: service(),
+    config: service(),
 
     // public attrs
     post: null,
 
-    selectedVisibility: computed('post.visibility', function () {
-        return this.get('post.visibility') || this.settings.get('defaultContentVisibility');
+    selectedVisibility: computed("post.visibility", function () {
+        return (
+            this.get("post.visibility") ||
+            this.settings.get("defaultContentVisibility")
+        );
     }),
 
     init() {
+        console.log("object");
         this._super(...arguments);
-        this.availableVisibilities = VISIBILITIES;
+        this.get("createVisibilities");
     },
+
+    createVisibilities: computed(
+        "config.tiers.tier1",
+        "config.tiers.tier2",
+        function () {
+            this.availableVisibilities = [
+                { label: "Public", name: "public" },
+                { label: this.get("config.tiers.tier1"), name: "members" },
+                { label: this.get("config.tiers.tier2"), name: "paid" },
+            ];
+        }
+    ),
 
     actions: {
         updateVisibility(newVisibility) {
-            this.post.set('visibility', newVisibility);
-        }
-    }
+            this.post.set("visibility", newVisibility);
+        },
+    },
 });
